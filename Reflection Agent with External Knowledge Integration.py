5c3b58de03ddd147dcb0475a6e8c3d952d548879
jknigel <ajknigel360@gmail.com>
2025-09-12 20:13:41 +0800

    Update 2.1

--- Reflection Agent with External Knowledge Integration.py
+++ Reflection Agent with External Knowledge Integration.py
@@ -74,13 +74,13 @@
 # 
 
 # %%
-%%capture
-%pip install langchain-openai==0.3.10
-%pip install langchain==0.3.21
-%pip install openai==1.68.2
-%pip install langchain-community==0.2.1
-%pip install  --upgrade langgraph
-%pip install langchain_community==0.3.24
+#%%capture
+#%pip install langchain-openai==0.3.10
+#%pip install langchain==0.3.21
+#%pip install openai==1.68.2
+#%pip install langchain-community==0.2.1
+#%pip install  --upgrade langgraph
+#%pip install langchain_community==0.3.24
 
 # %% [markdown]
 # ### Importing Required Libraries
